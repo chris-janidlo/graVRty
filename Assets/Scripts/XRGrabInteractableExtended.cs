b652b92448a9d2d6c6a183254f12457123d091ea
crass_sandwich <christopherjanidlo@gmail.com>
2022-01-14 09:28:27 -0600

    Configure the ability to hover-activate an Interactable, per (grab) Interactable

---
+++ Assets/Scripts/XRGrabInteractableExtended.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableExtended : XRGrabInteractable
+{
+    public bool AllowHover;
+
+    protected override void OnActivated (ActivateEventArgs args)
+    {
+        if (isSelected || AllowHover) base.OnActivated(args);
+    }
+
+    protected override void OnDeactivated (DeactivateEventArgs args)
+    {
+        if (isSelected || AllowHover) base.OnDeactivated(args);
+    }
+}

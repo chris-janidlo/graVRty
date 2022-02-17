11622855dea98e1756dfbfd4206d6df21fdf6697
crass_sandwich <christopherjanidlo@gmail.com>
2022-02-16 21:01:25 -0600

    Flashlight improvements

--- Assets/Scripts/Runtime/Interactables/Flashlight.cs
+++ Assets/Scripts/Runtime/Interactables/Flashlight.cs
@@ -8,33 +8,20 @@ namespace GraVRty.Interactables
 {
     public class Flashlight : MonoBehaviour
     {
-        [SerializeField] float m_LightStartSize, m_LightEndSize, m_LightStartTime, m_LightTransitionTime;
-        [SerializeField] Ease m_LightTransitionEase;
-
         [SerializeField] FlashlightLight m_LightPrefab;
         [SerializeField] Transform m_LightParent;
 
         FlashlightLight currentLight;
-        Tweener lightTweener;
 
         public void OnActivated (ActivateEventArgs args)
         {
             currentLight = Instantiate(m_LightPrefab, m_LightParent);
-            currentLight.SetSize(m_LightStartSize);
-
-            lightTweener = DOTween.To(() => currentLight.Size, currentLight.SetSize, m_LightEndSize, m_LightTransitionTime)
-                .SetEase(m_LightTransitionEase)
-                .SetDelay(m_LightStartTime)
-                .OnKill(() => lightTweener = null);
         }
 
         public void OnDeactivated (DeactivateEventArgs args)
         {
             currentLight.Kill();
             currentLight = null;
-
-            lightTweener?.Kill();
-            lightTweener = null;
         }
     }
 }
--- Assets/Scripts/Runtime/Interactables/FlashlightLight.cs
+++ Assets/Scripts/Runtime/Interactables/FlashlightLight.cs
@@ -1,22 +1,56 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace GraVRty.Interactables
 {
     public class FlashlightLight : MonoBehaviour
     {
-        public float Size { get; private set; }
-
-        public void SetSize (float size)
+        [Serializable]
+        public struct SizeSpec
         {
-            Size = size;
-            transform.localScale = new Vector3(size, size, 1f / size);
+            public float Radius, Length;
+
+            public static SizeSpec Lerp (SizeSpec a, SizeSpec b, float t)
+            {
+                return new SizeSpec
+                {
+                    Radius = Mathf.Lerp(a.Radius, b.Radius, t),
+                    Length = Mathf.Lerp(a.Length, b.Length, t)
+                };
+            }
+        }
+
+        [SerializeField] SizeSpec m_StartSize, m_EndSize;
+        [SerializeField] AnimationCurve m_SizeTransitionCurve;
+
+        IEnumerator Start ()
+        {
+            float currentTime = 0;
+            float totalTime = m_SizeTransitionCurve.keys.Last().time;
+
+            while (currentTime < totalTime)
+            {
+                setLerpedSize(m_SizeTransitionCurve.Evaluate(currentTime));
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+
+            setLerpedSize(m_SizeTransitionCurve.keys.Last().value);
         }
 
         public void Kill ()
         {
             Destroy(gameObject);
+        }
+
+        void setLerpedSize (float t)
+        {
+            SizeSpec size = SizeSpec.Lerp(m_StartSize, m_EndSize, t);
+            transform.localScale = new Vector3(size.Radius * 2, size.Radius * 2, size.Length);
         }
     }
 }

e86c8ffcdb56c51dd051311e4c82e8c72fa5f203
crass_sandwich <christopherjanidlo@gmail.com>
2022-02-21 23:31:04 -0600

    Occluded light beam collision

--- Assets/Scripts/Runtime/Interactables/FlashlightBeam.cs
+++ Assets/Scripts/Runtime/Interactables/FlashlightBeam.cs
@@ -3,12 +3,17 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+using crass;
 
 namespace GraVRty.Interactables
 {
     public class FlashlightBeam : MonoBehaviour
     {
+        public enum RaycastHitState
+        {
+            Missed, HitNonTarget, HitTarget
+        }
+
         [Serializable]
         public struct SizeSpec
         {
@@ -27,7 +32,13 @@ namespace GraVRty.Interactables
         [SerializeField] SizeSpec m_StartSize, m_EndSize;
         [SerializeField] AnimationCurve m_SizeTransitionCurve;
 
+        [SerializeField] int m_RaycastsPerFrame;
+        [SerializeField] LayerMask m_RaycastLayers;
+        [SerializeField] bool m_DrawDebugRays;
+        [SerializeField] EnumMap<RaycastHitState, Color> m_DebugRayColors;
+
         [SerializeField] Light m_Light;
+        [SerializeField] Transform m_GeometryTransformer, m_Geometry;
 
         IEnumerator Start ()
         {
@@ -49,6 +60,14 @@ namespace GraVRty.Interactables
             setLerpedSize(m_SizeTransitionCurve.keys.Last().value);
         }
 
+        void FixedUpdate ()
+        {
+            foreach (FlashlightBeamTarget target in getTargetsHitByBeam())
+            {
+                target.TrackBeamHit();
+            }
+        }
+
         public void Kill ()
         {
             Destroy(gameObject);
@@ -60,6 +79,57 @@ namespace GraVRty.Interactables
 
             m_Light.range = size.Length;
             m_Light.spotAngle = size.Angle;
+
+            // radius / length = tan(angle)
+            float triggerRadius = size.Length * Mathf.Tan(size.Angle * Mathf.Deg2Rad / 2);
+            m_GeometryTransformer.localScale = new Vector3(triggerRadius, triggerRadius, size.Length);
+        }
+
+        IEnumerable<FlashlightBeamTarget> getTargetsHitByBeam ()
+        {
+            HashSet<FlashlightBeamTarget> targetsAlreadyReturnedThisFrame = new HashSet<FlashlightBeamTarget>();
+
+            foreach (Vector3 point in getRaycastTargetPoints())
+            {
+                Vector3 difference = point - transform.position;
+
+                bool hit = Physics.Raycast(transform.position, difference.normalized, out RaycastHit hitInfo, difference.magnitude, m_RaycastLayers);
+                if (!hit)
+                {
+                    drawRay(transform.position, difference, RaycastHitState.Missed);
+                    continue;
+                }
+
+                FlashlightBeamTarget target = hitInfo.collider.GetComponent<FlashlightBeamTarget>();
+                if (target == null)
+                {
+                    drawRay(transform.position, difference, RaycastHitState.HitNonTarget);
+                    continue;
+                }
+
+                drawRay(transform.position, difference, RaycastHitState.HitTarget);
+
+                if (!targetsAlreadyReturnedThisFrame.Contains(target))
+                {
+                    targetsAlreadyReturnedThisFrame.Add(target);
+                    yield return target;
+                }
+            }
+        }
+
+        IEnumerable<Vector3> getRaycastTargetPoints ()
+        {
+            for (int i = 0; i < m_RaycastsPerFrame; i++)
+            {
+                yield return m_Geometry.TransformPoint(UnityEngine.Random.insideUnitCircle);
+            }
+        }
+
+        void drawRay (Vector3 start, Vector3 direction, RaycastHitState hitState)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            if (m_DrawDebugRays) Debug.DrawRay(start, direction, m_DebugRayColors[hitState]);
+#endif // UNITY_EDITOR || DEVELOPMENT_BUILD
         }
     }
 }
---
+++ Assets/Scripts/Runtime/Interactables/FlashlightBeamTarget.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GraVRty.Interactables
+{
+    public class FlashlightBeamTarget : MonoBehaviour
+    {
+        [SerializeField] UnityEvent m_HitByBeam;
+
+        public UnityEvent HitByBeam => m_HitByBeam;
+
+        public virtual void TrackBeamHit ()
+        {
+            HitByBeam.Invoke();
+            Debug.Log(gameObject.name + " was hit");
+        }
+    }
+}

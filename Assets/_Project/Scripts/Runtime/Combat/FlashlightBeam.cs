37ddaf51677db5a10a46418eee8129f12e083f6e
crass_sandwich <christopherjanidlo@gmail.com>
2022-04-26 23:18:01 -0500

    Configurable flashlight beam raycast direction
    Also:
    * Better naming convention: Geometry -> Cone Base Transform, Geometry Transformer -> Cone Scaler
    * When drawing debug rays, end them wherever they actually hit

--- Assets/_Project/Scripts/Runtime/Combat/FlashlightBeam.cs
+++ Assets/_Project/Scripts/Runtime/Combat/FlashlightBeam.cs
@@ -15,6 +15,11 @@ namespace GraVRty.Combat
             Missed, HitNonTarget, HitTarget
         }
 
+        public enum Direction
+        {
+            PointToBase, BaseToPoint
+        }
+
         [Serializable]
         public struct SizeSpec
         {
@@ -22,6 +27,7 @@ namespace GraVRty.Combat
         }
 
         [SerializeField] SizeSpec m_Size;
+        [SerializeField] Direction m_RayDirection;
         [SerializeField] int m_Raycasts;
 
         [SerializeField] LayerMask m_RaycastLayers;
@@ -29,7 +35,7 @@ namespace GraVRty.Combat
         [SerializeField] EnumMap<RaycastHitState, Color> m_DebugRayColors;
 
         [SerializeField] Light m_Light;
-        [SerializeField] Transform m_GeometryTransformer, m_Geometry;
+        [SerializeField] Transform m_ConeScaler, m_ConePointTransform, m_ConeBaseTransform;
 
         List<Vector3> circlePoints;
         Dictionary<FlashlightBeamTarget, BeamHitInfo> targetTracking;
@@ -53,22 +59,42 @@ namespace GraVRty.Combat
 
             foreach (Vector3 circlePoint in circlePoints)
             {
-                Vector3 worldPoint = m_Geometry.TransformPoint(circlePoint);
-                Vector3 difference = worldPoint - transform.position;
+                Vector3 circlePointInWorld = m_ConeBaseTransform.TransformPoint(circlePoint);
 
-                if (!Physics.Raycast(transform.position, difference.normalized, out RaycastHit rayHitInfo, difference.magnitude, m_RaycastLayers))
+                Vector3 startPosition, endPosition;
+                switch (m_RayDirection)
                 {
-                    drawRay(transform.position, difference, RaycastHitState.Missed);
+                    case Direction.PointToBase:
+                        startPosition = m_ConePointTransform.position;
+                        endPosition = circlePointInWorld;
+                        break;
+
+                    case Direction.BaseToPoint:
+                        startPosition = circlePointInWorld;
+                        endPosition = m_ConePointTransform.position;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"{nameof(m_RayDirection)} has unexpected {typeof(Direction).Name} value {m_RayDirection}");
+                }
+
+                Vector3 rayCastDifference = endPosition - startPosition;
+
+                if (!Physics.Raycast(startPosition, rayCastDifference.normalized, out RaycastHit rayHitInfo, rayCastDifference.magnitude, m_RaycastLayers))
+                {
+                    drawRay(startPosition, rayCastDifference, RaycastHitState.Missed);
                     continue;
                 }
+
+                Vector3 hitDifference = rayHitInfo.point - startPosition;
 
                 if (!rayHitInfo.collider.TryGetComponent(out FlashlightBeamTarget target))
                 {
-                    drawRay(transform.position, difference, RaycastHitState.HitNonTarget);
+                    drawRay(startPosition, hitDifference, RaycastHitState.HitNonTarget);
                     continue;
                 }
 
-                drawRay(transform.position, difference, RaycastHitState.HitTarget);
+                drawRay(startPosition, hitDifference, RaycastHitState.HitTarget);
 
                 if (!targetTracking.TryGetValue(target, out BeamHitInfo beamHitInfo))
                 {
@@ -91,7 +117,7 @@ namespace GraVRty.Combat
 
             // radius / length = tan(angle)
             float baseRadius = m_Size.Length * Mathf.Tan(m_Size.Angle * Mathf.Deg2Rad / 2);
-            m_GeometryTransformer.localScale = new Vector3(baseRadius, baseRadius, m_Size.Length);
+            m_ConeScaler.localScale = new Vector3(baseRadius, baseRadius, m_Size.Length);
         }
 
         void calculateCartesianPoints ()

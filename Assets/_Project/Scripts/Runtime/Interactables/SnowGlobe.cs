22f0adfe901924cde4094388ce1145865b361aa4
crass_sandwich <christopherjanidlo@gmail.com>
2022-04-29 20:25:32 -0500

    Initial focused flashlight beam implementation

--- Assets/_Project/Scripts/Runtime/Interactables/SnowGlobe.cs
+++ Assets/_Project/Scripts/Runtime/Interactables/SnowGlobe.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using GraVRty.CorePhysics;
+using GraVRty.Combat;
+using GraVRty.Loading;
 
 namespace GraVRty.Interactables
 {
     public class SnowGlobe : MonoBehaviour
     {
+        [Range(0, 1)]
+        [SerializeField] float m_BeamHitPercentageToTriggerFocusedBeam;
+        [SerializeField] float m_BeamCutoffFudge;
+
         [SerializeField] Rigidbody m_Rigidbody;
         [SerializeField] Transform m_InsidesParent;
         [SerializeField] Gravity m_Gravity;
+        [SerializeField] LoadableSemiEagerGameObjectPool FocusedFlashlightBeamPool;
 
         XRBaseController currentController;
+        FlashlightBeam currentFocusedFlashlightBeam;
 
         void Update ()
         {
@@ -36,6 +44,41 @@ namespace GraVRty.Interactables
         {
             m_Gravity.SetGravity(m_InsidesParent, 0);
             currentController = null;
+        }
+
+        public void OnBeamStay (BeamHitInfo beamHitInfo)
+        {
+            if (beamHitInfo.PercentageHit >= m_BeamHitPercentageToTriggerFocusedBeam)
+            {
+                FlashlightBeam.Dimensions dimensions = beamHitInfo.SourceBeam.CurrentDimensions;
+                dimensions.Length = Vector3.Distance(beamHitInfo.Centroid, beamHitInfo.SourceBeam.transform.position) + m_BeamCutoffFudge;
+                beamHitInfo.SourceBeam.SetDimensions(dimensions);
+
+                if (currentFocusedFlashlightBeam == null)
+                {
+                    currentFocusedFlashlightBeam = FocusedFlashlightBeamPool.Get<FlashlightBeam>(transform);
+                }
+            }
+            else
+            {
+                beamHitInfo.SourceBeam.ResetDimensions();
+                tryKillBeam();
+            }
+        }
+
+        public void OnBeamExited (FlashlightBeam flashlightBeam)
+        {
+            flashlightBeam.ResetDimensions();
+            tryKillBeam();
+        }
+
+        void tryKillBeam ()
+        {
+            if (currentFocusedFlashlightBeam != null)
+            {
+                FocusedFlashlightBeamPool.Release(currentFocusedFlashlightBeam);
+                currentFocusedFlashlightBeam = null;
+            }
         }
     }
 }

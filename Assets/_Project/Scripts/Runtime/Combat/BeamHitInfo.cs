7f90e75926b94bb88d2bfb881ccb77472ad0b31a
crass_sandwich <christopherjanidlo@gmail.com>
2022-05-27 21:57:42 -0500

    Performance and encapsulation improvements in BeamHitInfo

--- Assets/_Project/Scripts/Runtime/Combat/BeamHitInfo.cs
+++ Assets/_Project/Scripts/Runtime/Combat/BeamHitInfo.cs
@@ -6,25 +6,29 @@ namespace GraVRty.Combat
 {
     public struct BeamHitInfo
     {
-        public int RayHitCount, TotalRaycasts;
-        public Vector3 Centroid;
+        public int RayHitCount { get; private set; }
+        public int TotalRaycasts { get; private set; }
 
-        public FlashlightBeam SourceBeam;
+        public FlashlightBeam SourceBeam { get; private set; }
 
         public float PercentageHit => (float) RayHitCount / TotalRaycasts;
+        public Vector3 Centroid => cumulativeHitPosition / TotalRaycasts;
+
+        Vector3 cumulativeHitPosition;
 
         public BeamHitInfo (FlashlightBeam sourceBeam, int raycasts)
         {
             SourceBeam = sourceBeam;
             TotalRaycasts = raycasts;
             RayHitCount = 0;
-            Centroid = Vector3.zero;
+
+            cumulativeHitPosition = Vector3.zero;
         }
 
         public BeamHitInfo PlusRayHit (Vector3 position)
         {
             RayHitCount++;
-            Centroid += (position - Centroid) / RayHitCount; // cumulative average
+            cumulativeHitPosition += position;
             return this;
         }
     }

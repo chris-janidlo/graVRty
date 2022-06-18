e0580fbc4d7caf4162fb0025dc3495d985f725b2
crass_sandwich <christopherjanidlo@gmail.com>
2022-06-17 20:13:17 -0500

    Crude but effective focused beam smoothing
    Also rename ConvergingFlashlightBeamVisual to FocusedFlashlightBeamVisual to denote that it's more specific than just any flashlight beam that converges to a point

---
+++ Assets/_Project/Scripts/Runtime/Flashlights/FocusedFlashlightBeamVisual.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraVRty.Flashlights
+{
+    public class FocusedFlashlightBeamVisual : AFlashlightBeamVisual
+    {
+        [SerializeField] ConvergingLight m_Light;
+
+        Vector3 previousForward;
+
+        // this is in LateUpdate so that this script can react to other scripts changing transform.forward during Update
+        void LateUpdate ()
+        {
+            transform.forward = Vector3.Slerp(transform.forward, previousForward, 0.5f);
+            previousForward = transform.forward;
+        }
+
+        public override void SetDimensions (Cone cone)
+        {
+            m_Light.Dimensions = cone;
+        }
+    }
+}
